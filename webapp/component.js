958067b2d4149a4f689b7756bb6f7764146d0d5c
Viih Neris <viihneris049@gmail.com>
2023-02-23 13:21:12 -0300

    Step 8-11, layout

--- webapp/component.js
+++ webapp/component.js
@@ -1,41 +1,73 @@
-sap.ui.define(
-  [
+// sap.ui.define(
+//   [
+//     "sap/ui/core/UIComponent",
+//     "sap/ui/model/json/JSONModel",
+//     "sap/ui/model/resource/ResourceModel",
+//   ],
+//   function (UIComponent, JSONModel, ResourceModel) {
+//     "use strict";
+//     return UIComponent.extend("sap.ui.walkthrough.Component", {
+//       metadata: {
+//         /*
+//             // This code has been isolated in "manifest.js" (SOLID)
+
+//             rootView: {
+//             viewName: "sap.ui.demo.walkthrough.view.App",
+//             type: "XML",
+//             async: true,
+//             id: "app",
+//         */
+//         manifest: "json",
+//       },
+//       init: function () {
+//         // call the init function of the parent
+//         UIComponent.prototype.init.apply(this, arguments);
+//         // set data models
+//         var oData = {
+//           recipient: {
+//             name: "UI5",
+//           },
+//         };
+//         var oModel = new JSONModel(oData);
+//         this.setModel(oModel);
+
+//         /*
+//         // This code has been isolated in "manifest.js" (SOLID)
+//             //set i18n Model
+
+//             var i18nModel = new ResourceModel({
+//             bundleName: "sap.ui.demo.walkthrough.i18n.i18n",
+//             supportedLocales: [""],
+//             fallbackLocal: "",
+//             });
+//             this.setModel(i18nModel, "i18n");
+//         */
+//       },
+//     });
+//   }
+// );
+
+sap.ui.define([
     "sap/ui/core/UIComponent",
     "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel",
-  ],
-  function (UIComponent, JSONModel, ResourceModel) {
+    "sap/ui/model/resource/ResourceModel"
+], function (UIComponent, JSONModel, ResourceModel) {
     "use strict";
-    return UIComponent.extend("sap.ui.walkthrough.Component", {
-      metadata: {
-        rootView: {
-          viewName: "sap.ui.demo.walkthrough.view.App",
-          type: "XML",
-          async: true,
-          id: "app",
+    return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
+        metadata : {
+            manifest: "json"
         },
-      },
-      init: function () {
-        // call the init function of the parent
-        UIComponent.prototype.init.apply(this, arguments);
-        // set data models
-        var oData = {
-          recipient: {
-            name: "UI5",
-          },
-        };
-        var oModel = new JSONModel(oData);
-        this.setModel(oModel);
-
-        //set i18n Model
-
-        var i18nModel = new ResourceModel({
-          bundleName: "sap.ui.demo.walkthrough.i18n.i18n",
-          supportedLocales: [""],
-          fallbackLocal: "",
-        });
-        this.setModel(i18nModel, "i18n");
-      },
+        init : function () {
+            // call the init function of the parent
+            UIComponent.prototype.init.apply(this, arguments);
+            // set data models
+            var oData = {
+                recipient: {
+                    name: "UI5"
+                }
+            };
+            var oModel = new JSONModel(oData);
+            this.setModel(oModel);
+        }
     });
-  }
-);
+});
--- webapp/index.js
+++ webapp/index.js
@@ -1,3 +1,5 @@
+// <--- FILE DELETED BECAUSE IT WAS NO LONGER NEEDED --->
+
 // sap.ui.define([
 //     "sap/m/Text"
 
@@ -9,32 +11,32 @@
 //     }).placeAt("content")
 // })
 
-/*
-sap.ui.define([
-    "sap/ui/core/mvc/XMLView"
+// /*
+// sap.ui.define([
+//     "sap/ui/core/mvc/XMLView"
 
-], function (XMLView) {
-    "use strict";
+// ], function (XMLView) {
+//     "use strict";
 
-    XMLView.create({
-        viewName:"sap.ui.demo.walkthrough.view.App"
-    }).then(function (oView) {
-        oView.placeAt("content");
-    })
-})
-*/
+//     XMLView.create({
+//         viewName:"sap.ui.demo.walkthrough.view.App"
+//     }).then(function (oView) {
+//         oView.placeAt("content");
+//     })
+// })
+// */
 
-sap.ui.define(
-  ["sap/ui/core/ComponentContainer"],
-  function (ComponentContainer) {
-    "use strict";
+// sap.ui.define(
+//   ["sap/ui/core/ComponentContainer"],
+//   function (ComponentContainer) {
+//     "use strict";
 
-    new ComponentContainer({
-      name: "sap.ui.demo.walkthrough",
-      settings: {
-        id: "walkthrough",
-      },
-      async: true,
-    }).placeAt("content");
-  }
-);
+//     new ComponentContainer({
+//       name: "sap.ui.demo.walkthrough",
+//       settings: {
+//         id: "walkthrough",
+//       },
+//       async: true,
+//     }).placeAt("content");
+//   }
+// );
