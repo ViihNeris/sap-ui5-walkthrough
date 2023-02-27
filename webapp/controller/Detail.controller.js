c5485286c35704643b69a6fae61fc1801512eea8
Viih Neris <viihneris049@gmail.com>
2023-02-26 22:22:29 -0300

    Step 33: Routing Back and History
    Adicionando função de voltar a página anterior em 'Details'

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,19 +1,32 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/core/UIComponent"
-], function (Controller, UIComponent) {
+    "sap/ui/core/UIComponent",
+    "sap/ui/core/routing/History"
+], function (Controller, UIComponent, History) {
     "use strict";
 
     return Controller.extend("sap.ui.demo.walkthrough.controller.Detail", {
         onInit: function () {
             var oRouter = sap.ui.core.UIComponent.getRouterFor(this)
-            oRouter.getRoute("detail").attachPatternMatched(this._onObjectMatch, this);
+            oRouter.getRoute("detail").attachPatternMatched(this._onObjectMatched, this);
         },
-        _onObjectMatch: function (oEvent) {
+        _onObjectMatched: function (oEvent) {
             this.getView().bindElement({
                 path: "/" + window.decodeURIComponent(oEvent.getParameter("arguments").invoicePath),
                 model: "invoice"
             })
+
+        },
+        onNavBack: function () {
+            var oHistory = History.getInstance();
+            var sPreviousHash = oHistory.getPreviousHash();
+
+            if (sPreviousHash !== undefined) {
+                window.history.go(-1)
+            } else {
+                var oRouter = UIComponent.getRouterFor(this);
+                oRouter.navTo("overview", {}, true)
+            }
         }
     })
 
