d69514e176fbe233e8f4c2d08b33a98fbd32b3a5
Viih Neris <viihneris049@gmail.com>
2023-02-27 13:14:20 -0300

    Translation: Portuguese
    Traduzindo as informações do layout para o português brasileiro via JSON e i18n.

--- webapp/test/unit/model/formatter.js
+++ webapp/test/unit/model/formatter.js
@@ -40,19 +40,19 @@ sap.ui.define(
       // Assert
       assert.strictEqual(
         fnIsolatedFormatter("A"),
-        "New",
+        "Novo",
         "The long text for status A is correct"
       );
 
       assert.strictEqual(
         fnIsolatedFormatter("B"),
-        "In Progress",
+        "Em Progresso",
         "The long text for status B is correct"
       );
 
       assert.strictEqual(
         fnIsolatedFormatter("C"),
-        "Done",
+        "Concluído",
         "The long text for status C is correct"
       );
 
